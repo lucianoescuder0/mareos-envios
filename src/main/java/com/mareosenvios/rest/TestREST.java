43227cbe1c9fd89378bb68cffb13d49979af164a
luciano-silohub <lucianoescudero98@gmail.com>
2024-10-21 00:09:12 -0300

    Config swagger

--- src/main/java/com/mareosenvios/rest/TestREST.java
+++ src/main/java/com/mareosenvios/rest/TestREST.java
@@ -5,12 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping("/prueba")
+@Api(value = "Customer Management System", description = "Operaciones relacionadas con los clientes")
 public class TestREST {
 
-
+    @ApiOperation(value = "Test")
     @GetMapping()
     public ResponseEntity<ResponseServiceDTO> test() {
         ResponseServiceDTO response = new ResponseServiceDTO();
